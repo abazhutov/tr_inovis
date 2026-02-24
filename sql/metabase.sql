b7236bbf3514914c881769ee356b2fe47312c2b1
abazhutov <abazhutov@mail.ru>
2026-02-24 12:43:21 +0400

    fix products dashboard query

--- sql/metabase.sql
+++ sql/metabase.sql
@@ -5,8 +5,8 @@ join dwh_dim_customers c on s.customer_id = c.id
 -- join dwh_dim_products p on s.product_id = p.id
 group by 1
 
--- avg sales by product for last 30 days
-select p.name, avg(s.qty) as avg 
+--avg sales by product for last 30 days
+select p.name, avg(s.qty) as avagadro 
 from dwh_fact_sales s
 -- join dwh_dim_customers c on s.customer_id = c.id
 join dwh_dim_products p on s.product_id = p.id
